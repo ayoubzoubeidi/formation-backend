eb890d5356b0558dc42ff82d12f1eb6c20768161
ayoub <ayoubzoubeidi@gmail.com>
2022-02-09 21:23:10 +0100

    initial project commit.

---
+++ src/main/java/com/isi/formation/FormationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.isi.formation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormationBackendApplication.class, args);
+    }
+
+}
